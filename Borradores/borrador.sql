b0a1a2d5036fa39b58849d486c193f5df68a91d7
Francisco Bahamondes Castro <fbahamondesc@icafal.cl>
2020-03-27 14:03:09 -0300

    20200327

--- Borradores/borrador.sql
+++ Borradores/borrador.sql
@@ -1,85 +1,117 @@
 DECLARE @emp_cod CHAR(6) = '0102'
 
-SELECT DISTINCT A.RefNbr
-				,A.BatNbr
-				,A.PerPost
-				,A.apdoc_id
-				,Null AS ardoc_id
-				,A.cer_id
-				,Null as DocType
-				,CAST(A.TranTot AS numeric) AS TranTot
-				,CAST(A.TaxAmt00 AS numeric) AS TaxAmt00
-				,CAST(B.TranAmt AS numeric) AS TaxAmt01
-				,CAST(ISNULL(A.TaxAmt00,0) + ISNULL(B.TranAmt,0) AS numeric) AS TaxTot
-			FROM
-			/* Agrega montos de impuesto desde DOC_APTran */
-			(
-				SELECT A.apdoc_id
-					,LC.cer_id
-					,A.RefNbr
-					,A.BatNbr
-					,A.PerPost
-					,A.TranAmt AS TranTot
-					,CASE 
-						WHEN A.TranAmt < 0
-							THEN (A.CuryTaxAmt00 + A.CuryTaxAmt01 + A.CuryTaxAmt02) * - 1
-						ELSE (A.CuryTaxAmt00 + A.CuryTaxAmt01 + A.CuryTaxAmt02)
-						END AS TaxAmt00
-				FROM LOB_CERRADO_DETALLE LCD WITH (NOLOCK)
-				INNER JOIN LOB_CERRADO LC ON LCD.Cer_Id = LC.cer_id
-				INNER JOIN DOC_APTran A  ON 1=1
-					AND A.apdoc_id IN (LCD.Aux_apdoc_cl_id, LCD.Aux_apdoc_id)
-					AND A.TranAmt = LC.DrAmt - LC.CrAmt
-					AND A.BatNbr = LCD.Lote
-					AND A.RefNbr = LCD.RefNbr
-				WHERE A.User1 != '' 
-					AND LCD.Emp_Cod = @emp_cod
-					AND 'EXENTO' NOT IN (A.taxId00, A.taxId01, A.taxId02)
-					AND NOT( A.Acct = '110902' 
-					AND ('ESPECIFICO' IN (A.taxId00, A.taxId01, A.taxId02) 
-					OR A.TranDesc = 'Impuesto Especifico'))
-				--GROUP BY A.apdoc_id	
-				--	,A.RefNbr
-				--	,A.BatNbr
-				--	,A.PerPost
-				--	,LC.cer_id
-			) A
-			/* Agrega impuesto especifico */
-			LEFT JOIN DOC_APTran B WITH (NOLOCK) ON B.apdoc_id = A.apdoc_id 
-				AND B.Acct = '110902' 
-				AND B.TranAmt != 0 
-				AND ('ESPECIFICO' IN (B.taxId00, B.taxId01, B.taxId02) 
-					OR B.TranDesc = 'Impuesto Especifico')
-			WHERE NOT (A.TaxAmt00 = 0)
+--SELECT DISTINCT A.RefNbr
+--			,A.BatNbr
+--			,A.PerPost
+--			,A.apdoc_id
+--			,Null AS ardoc_id
+--			,A.cer_id
+--			,Null as DocType
+--			,CAST(A.TranTot AS numeric) AS TranTot
+--			,CAST(A.TaxAmt00 AS numeric) AS TaxAmt00
+--			,CAST(B.TranAmt AS numeric) AS TaxAmt01
+--			,CAST(ISNULL(A.TaxAmt00,0) + ISNULL(B.TranAmt,0) AS numeric) AS TaxTot
+--		FROM
+--		/* Agrega montos de impuesto desde DOC_APTran */
+--		(
+--			SELECT A.apdoc_id
+--				,LC.cer_id
+--				,A.RefNbr
+--				,A.BatNbr
+--				,A.PerPost
+--				,A.TranAmt AS TranTot
+--				,CASE 
+--					WHEN A.TranAmt < 0
+--						THEN (A.CuryTaxAmt00 + A.CuryTaxAmt01 + A.CuryTaxAmt02) * - 1
+--					ELSE (A.CuryTaxAmt00 + A.CuryTaxAmt01 + A.CuryTaxAmt02)
+--					END AS TaxAmt00
+--			FROM LOB_CERRADO_DETALLE LCD WITH (NOLOCK)
+--			INNER JOIN LOB_CERRADO LC ON LCD.Cer_Id = LC.cer_id
+--			INNER JOIN DOC_APTran A  ON 1=1
+--				AND A.apdoc_id IN (LCD.Aux_apdoc_cl_id, LCD.Aux_apdoc_id)
+--				AND A.TranAmt = LC.DrAmt - LC.CrAmt
+--				AND A.BatNbr = LCD.Lote
+--				AND A.RefNbr = LCD.RefNbr
+--			WHERE A.User1 != '' 
+--				AND LCD.Emp_Cod = @emp_cod
+--				AND 'EXENTO' NOT IN (A.taxId00, A.taxId01, A.taxId02)
+--				AND NOT( A.Acct = '110902' 
+--				AND ('ESPECIFICO' IN (A.taxId00, A.taxId01, A.taxId02) 
+--				OR A.TranDesc = 'Impuesto Especifico'))
+--			--GROUP BY A.apdoc_id	
+--			--	,A.RefNbr
+--			--	,A.BatNbr
+--			--	,A.PerPost
+--			--	,LC.cer_id
+--		) A
+--		/* Agrega impuesto especifico */
+--		LEFT JOIN DOC_APTran B WITH (NOLOCK) ON B.apdoc_id = A.apdoc_id 
+--			AND B.Acct = '110902' 
+--			AND B.TranAmt != 0 
+--			AND ('ESPECIFICO' IN (B.taxId00, B.taxId01, B.taxId02) 
+--				OR B.TranDesc = 'Impuesto Especifico')
+--		WHERE NOT (A.TaxAmt00 = 0)
 
-			UNION
-			/* Agrega impuestos de modulos AR */
-			SELECT DISTINCT a.refnbr
-					,a.batnbr
-					,A.Perpost
-					,NULL AS apdoc_id
-					,B.ardoc_id
-					,LC.cer_id
-					,A.DocType
-					,CAST(B.tranamt AS numeric) as TranTot
-					,CASE WHEN A.DocType = 'CM' THEN 
-						CAST(B.CuryTaxAmt00 + B.CuryTaxAmt01 + B.CuryTaxAmt02 AS numeric) * -1 
-						ELSE CAST(B.CuryTaxAmt00 + B.CuryTaxAmt01 + B.CuryTaxAmt02 AS numeric) END AS TaxAmt00
-					,NULL as TaxAmt01
-					,CAST(B.CuryTaxAmt00 + B.CuryTaxAmt01 + B.CuryTaxAmt02 AS numeric) AS TaxTot
-				FROM LOB_CERRADO LC
-				INNER JOIN LOB_CERRADO_DETALLE LCD ON LC.cer_id = LCD.Cer_Id
-				INNER JOIN DOC_ARDOC A WITH (NOLOCK) ON 1=1
-					AND A.refnbr = LCD.RefNbr 
-					AND LCD.Lote = A.batnbr 
-				INNER JOIN DOC_ARTRAN B WITH (NOLOCK) ON A.ardoc_id = B.ardoc_id
-					AND ABS(LC.DrAmt - LC.CrAmt) = CAST(B.tranamt AS numeric)
-				WHERE B.user1 != '' 
-					AND (A.TaxTot00 + A.TaxTot01 + A.TaxTot02) != 0
-					AND LCD.Emp_Cod = @emp_cod
-					AND 'EXENTO' NOT IN (A.taxId00, A.taxId01, A.taxId02)
-					AND LCD.Modulo = 'AR' 
+--		--UNION
+--		--SELECT * FROM 
+
+--		UNION
+--		/* Agrega impuestos de modulos AR */
+--		SELECT DISTINCT a.refnbr
+--				,a.batnbr
+--				,A.Perpost
+--				,NULL AS apdoc_id
+--				,B.ardoc_id
+--				,LC.cer_id
+--				,A.DocType
+--				,CAST(B.tranamt AS numeric) as TranTot
+--				,CASE WHEN A.DocType = 'CM' THEN 
+--					CAST(B.CuryTaxAmt00 + B.CuryTaxAmt01 + B.CuryTaxAmt02 AS numeric) * -1 
+--					ELSE CAST(B.CuryTaxAmt00 + B.CuryTaxAmt01 + B.CuryTaxAmt02 AS numeric) END AS TaxAmt00
+--				,NULL as TaxAmt01
+--				,CAST(B.CuryTaxAmt00 + B.CuryTaxAmt01 + B.CuryTaxAmt02 AS numeric) AS TaxTot
+--			FROM LOB_CERRADO LC
+--			INNER JOIN LOB_CERRADO_DETALLE LCD ON LC.cer_id = LCD.Cer_Id
+--			INNER JOIN DOC_ARDOC A WITH (NOLOCK) ON 1=1
+--				AND A.refnbr = LCD.RefNbr 
+--				AND LCD.Lote = A.batnbr 
+--			INNER JOIN DOC_ARTRAN B WITH (NOLOCK) ON A.ardoc_id = B.ardoc_id
+--				AND ABS(LC.DrAmt - LC.CrAmt) = CAST(B.tranamt AS numeric)
+--			WHERE B.user1 != '' 
+--				AND (A.TaxTot00 + A.TaxTot01 + A.TaxTot02) != 0
+--				AND LCD.Emp_Cod = @emp_cod
+--				AND 'EXENTO' NOT IN (A.taxId00, A.taxId01, A.taxId02)
+--				AND LCD.Modulo = 'AR' 
 
 					
-select * from DOC_APTran where 1=1
-	and apdoc_id = 15785344
+--select * from DOC_APTran where 1=1
+--	and apdoc_id = 15785344
+
+SELECT LCD.InvtID as Insumo
+	,LCD.Aux_Glosa
+	,LCD.TipoDoc
+	,LCD.TipoMov
+	,LCD.RefNbr
+	,LCD.DocDesc
+	,LC.DrAmt - LC.CrAmt as MontoNeto
+	,APT.TranAmt
+	,LCD.TaxAmt
+	,(APT.CuryTaxAmt00 + APT.CuryTaxAmt01 + APT.CuryTaxAmt02) AS TAX
+	,LCD.Aux_apdoc_id
+	,LCD.Aux_apdoc_cl_id
+	FROM LOB_CERRADO_DETALLE LCD
+	INNER JOIN LOB_CERRADO LC ON LCD.Cer_Id = LC.cer_id
+	INNER JOIN LOB_PERIODO LP ON LC.per_id = LP.per_id
+	INNER JOIN DOC_APTran APT ON APT.apdoc_id IN (LCD.Aux_apdoc_id,LCD.Aux_apdoc_cl_id)
+	WHERE 1=1
+		AND LCD.Emp_Cod = @emp_cod
+		--AND TaxAmt != 0
+		AND LC.obra_cod = 'II10'
+		AND LCD.InvtID = '99402'
+		--AND LC.RefNbr = '034799'
+		AND APT.User1 != ''
+		AND 'EXENTO' NOT IN (APT.taxId00, APT.taxId01, APT.taxId02)
+	order by LCD.TipoDoc DESC
+		,LCD.RefNbr asc
+		,LP.per_periodo desc
+
--- Borradores/ejecuta_sp.sql
+++ Borradores/ejecuta_sp.sql
@@ -1,34 +1,57 @@
-SELECT * FROM LOB_CERRADO_DETALLE
-WHERE 1=1
-	AND InvtID = '99402'
-	AND RefNbr = '034799'
+DECLARE @apdoc_id AS INT = 15785323
 
-	select * from LOB_IMPUESTOS
-	where apdoc_id = 15785344
-SELECT * FROM LOB_CERRADO WHERE cer_id IN (
-1717755)
+--SELECT * FROM LOB_CERRADO_DETALLE
+--WHERE 1=1
+--	AND InvtID = '99402'
+--	AND RefNbr = '034799'
 
-
-exec PA_ProcesoCarga_Lob_Impuestos_Por_Empresa '0102'
-EXEC PA_ProcesoCarga_LobResumenDetalle '0102'
-TRUNCATE TABLE LOB_RESUMEN_DETALLE
-SELECT * FROM DOC_ARDOC WHERE 'EXENTO' IN (TaxId00,TaxID01,TaxId02)
-
-SELECT A.Cer_Id 
-	,B.DrAmt - B.CrAmt AS NETO
-	,A.TaxAmt
-	,(B.DrAmt - B.CrAmt) * 0.19 AS Imp
-FROM LOB_CERRADO_DETALLE A
-inner join LOB_CERRADO B ON A.Cer_Id = B.cer_id
-WHERE A.TaxAmt IS NOT NULL AND A.TaxAmt != 0
---UPDATE LOB_CERRADO_DETALLE SET TaxAmt = NULL, TaxAmt_uf = NULL WHERE Emp_Cod = '0101'
-SELECT * FROM DOC_APTran WHERE apdoc_id in (5470239) and User2 = 'IQ04'
-SELECT * FROM LOB_CERRADO_DETALLE WHERE Cer_Id IN (2657961
-,2633232
-)
-SELECT * FROM LOB_CERRADO WHERE Cer_Id IN (2633232
-,2633231
-)
+--	select * from LOB_IMPUESTOS
+--	where apdoc_id = 15785344
+--SELECT * FROM LOB_CERRADO WHERE cer_id IN (
+--1717755)
 
 
-SELECT * FROM LOB_CERRADO_DETALLE WHERE 15785344 in (Aux_apdoc_cl_id, Aux_apdoc_id)
+--exec PA_ProcesoCarga_Lob_Impuestos_Por_Empresa '0102'
+--EXEC PA_ProcesoCarga_LobResumenDetalle '0102'
+--TRUNCATE TABLE LOB_RESUMEN_DETALLE
+--SELECT * FROM DOC_ARDOC WHERE 'EXENTO' IN (TaxId00,TaxID01,TaxId02)
+
+--SELECT A.Cer_Id 
+--	,B.DrAmt - B.CrAmt AS NETO
+--	,A.TaxAmt
+--	,(B.DrAmt - B.CrAmt) * 0.19 AS Imp
+--FROM LOB_CERRADO_DETALLE A
+--inner join LOB_CERRADO B ON A.Cer_Id = B.cer_id
+--WHERE A.TaxAmt IS NOT NULL AND A.TaxAmt != 0
+----UPDATE LOB_CERRADO_DETALLE SET TaxAmt = NULL, TaxAmt_uf = NULL WHERE Emp_Cod = '0101'
+--SELECT * FROM DOC_APTran WHERE apdoc_id in (15785344)
+
+--SELECT a.Lote, c.BatNbr, b.DrAmt, b.CrAmt, * FROM LOB_CERRADO_DETALLE a
+--inner join LOB_CERRADO b on a.Cer_Id = b.cer_id
+--inner join DOC_APTran c on c.apdoc_id in (a.Aux_apdoc_cl_id, a.Aux_apdoc_id)
+--	and a.RefNbr = c.RefNbr
+--	--and a.Lote = c.BatNbr
+--	and ABS(b.DrAmt - b.CrAmt) = c.TranAmt
+--WHERE 15785344 in (a.Aux_apdoc_cl_id, a.Aux_apdoc_id)
+
+
+--SELECT * FROM LOB_CERRADO WHERE Cer_Id IN (2633232
+--,2633231
+--)
+
+
+SELECT lcd.TaxAmt 
+	,lc.DrAmt - lc.CrAmt as MontoNeto
+	, lc.InvtID
+	,lc.TipoMov
+FROM LOB_CERRADO_DETALLE lcd
+inner join LOB_CERRADO lc on lcd.Cer_Id = lc.cer_id
+WHERE @apdoc_id in (Aux_apdoc_cl_id, Aux_apdoc_id)
+
+--SELECT * FROM DOC_APTran A 
+--INNER JOIN DOC_APDoc B ON A.apdoc_id = B.apdoc_id
+--WHERE A.apdoc_id = 15785323
+--AND A.User1 != ''
+
+select * from DOC_APTran where apdoc_id = @apdoc_id
+--select * from DOC_APDoc where apdoc_id = @apdoc_id
